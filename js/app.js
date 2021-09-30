a6f44344a160f2e1405819a6ba3eb167ca71844b
retamozok <matiasagustin.1823@gmail.com>
2021-09-30 10:59:03 -0300

    30-septiembre

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log ("hola mundo")
+document.write ("<h3>hola muuundo<h3>")
+
+let maxi = "chau"
+let maxi2 = "buenos dias"
+console.log (maxi)
+maxi = "hasta mañana"
+console.log (maxi)
+console.log (maxi2)
+const PI = 3.14
+console.log (PI)
+let numero1 = 45
+console.log (numero1)
+numero1 = 50
+console.log (numero1)
+const MES = "julio"
+console.log (MES)
